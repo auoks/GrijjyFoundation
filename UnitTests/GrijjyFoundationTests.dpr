b77b8350da3b6f067145e44f509637f519cdaac1
auoks <auoks@qq.com>
2024-04-25 16:45:01 +0800

    comment

--- UnitTests/GrijjyFoundationTests.dpr
+++ UnitTests/GrijjyFoundationTests.dpr
@@ -1,61 +1,66 @@
 program GrijjyFoundationTests;
 
-{$IFNDEF TESTINSIGHT}
-{$APPTYPE CONSOLE}
+{$IFNDEF TESTINSIGHT} // If the TESTINSIGHT symbol is not defined
+{$APPTYPE CONSOLE}   // The application type is console
 {$ENDIF}
 
-{$STRONGLINKTYPES ON}
+{$STRONGLINKTYPES ON} // Enable strong linking types
 
-uses
-  System.SysUtils,
-  DUnitX.TestFramework,
-  DUnitX.Loggers.Console,
-  DUnitX.Loggers.Xml.NUnit,
-  Tests.Grijjy.Collections.Base in 'Tests\Tests.Grijjy.Collections.Base.pas',
-  Tests.Grijjy.Collections.Sets in 'Tests\Tests.Grijjy.Collections.Sets.pas',
-  Tests.Grijjy.Collections.RingBuffer in 'Tests\Tests.Grijjy.Collections.RingBuffer.pas',
-  Tests.Grijjy.Collections.Lists in 'Tests\Tests.Grijjy.Collections.Lists.pas',
-  Tests.Grijjy.Collections.Dictionaries in 'Tests\Tests.Grijjy.Collections.Dictionaries.pas',
-  Tests.Grijjy.Bson in 'Tests\Tests.Grijjy.Bson.pas',
-  Tests.Grijjy.Bson.IO in 'Tests\Tests.Grijjy.Bson.IO.pas',
-  Tests.Grijjy.Bson.Serialization in 'Tests\Tests.Grijjy.Bson.Serialization.pas',
-  Tests.Grijjy.ProtocolBuffers in 'Tests\Tests.Grijjy.ProtocolBuffers.pas',
-  Tests.Grijjy.PropertyBag in 'Tests\Tests.Grijjy.PropertyBag.pas';
+uses // Import the following units
+  System.SysUtils, // For basic system utilities
+  DUnitX.TestFramework, // For the DUnitX test framework
+  DUnitX.Loggers.Console, // For logging to the console
+  DUnitX.Loggers.Xml.NUnit, // For generating NUnit compatible XML output
+  Tests.Grijjy.Collections.Base, // Test unit for Grijjy collections (base)
+  Tests.Grijjy.Collections.Sets, // Test unit for Grijjy collections (sets)
+  Tests.Grijjy.Collections.RingBuffer, // Test unit for Grijjy collections (ring buffer)
+  Tests.Grijjy.Collections.Lists, // Test unit for Grijjy collections (lists)
+  Tests.Grijjy.Collections.Dictionaries, // Test unit for Grijjy collections (dictionaries)
+  Tests.Grijjy.Bson, // Test unit for Grijjy BSON
+  Tests.Grijjy.Bson.IO, // Test unit for Grijjy BSON I/O
+  Tests.Grijjy.Bson.Serialization, // Test unit for Grijjy BSON serialization
+  Tests.Grijjy.ProtocolBuffers, // Test unit for Grijjy Protocol Buffers
+  Tests.Grijjy.PropertyBag; // Test unit for Grijjy Property Bag
 
 var
-  runner : ITestRunner;
-  results : IRunResults;
-  logger : ITestLogger;
-  nunitLogger : ITestLogger;
+  runner : ITestRunner; // The test runner
+  results : IRunResults; // The test results
+  logger : ITestLogger; // The test logger
+  nunitLogger : ITestLogger; // The NUnit compatible XML logger
 begin
-{$IFDEF TESTINSIGHT}
-  TestInsight.DUnitX.RunRegisteredTests;
+{$IFDEF TESTINSIGHT} // If the TESTINSIGHT symbol is defined
+  TestInsight.DUnitX.RunRegisteredTests; // Run registered tests for TestInsight
   exit;
 {$ENDIF}
   try
-    //Check command line options, will exit if invalid
+    // Check command line options and exit if invalid
     TDUnitX.CheckCommandLine;
-    //Create the test runner
+
+    // Create the test runner
     runner := TDUnitX.CreateRunner;
-    //Tell the runner to use RTTI to find Fixtures
+
+    // Tell the runner to use RTTI to find Fixtures
     runner.UseRTTI := True;
-    //tell the runner how we will log things
-    //Log to the console window
+
+    // Tell the runner how to log things
+    // Log to the console window
     logger := TDUnitXConsoleLogger.Create(true);
     runner.AddLogger(logger);
 
-    //Generate an NUnit compatible XML File
+    // Generate an NUnit compatible XML file
     nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
     runner.AddLogger(nunitLogger);
-    runner.FailsOnNoAsserts := False; //When true, Assertions must be made during tests;
+    runner.FailsOnNoAsserts := False; // When true, Assertions must be made during tests
 
-    //Run tests
+    // Run tests
     results := runner.Execute;
+
+    // Exit with an error code if not all tests passed
     if not results.AllPassed then
       System.ExitCode := EXIT_ERRORS;
 
-    {$IFNDEF CI}
-    //We don't want this happening when running under CI.
+    {$IFNDEF CI} // If not running under CI
+    // Pause the console window when done
     if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
     begin
       System.Write('Done.. press <Enter> key to quit.');
@@ -67,3 +72,4 @@ begin
       System.Writeln(E.ClassName, ': ', E.Message);
   end;
 end.
+
